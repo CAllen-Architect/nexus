fff9d7217cfbc46b4537727aedc631df80f18edb
Carl <carl.e.allen@gmail.com>
2026-05-15 22:15:50 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CAllen | Atlassian Systems Architecture"",
+    pageTitle: "CAllen | Atlassian Systems Architecture",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
