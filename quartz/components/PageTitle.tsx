9659b8a26c77640bbfe54a059b5d8cd112124de6
Carl <carl.e.allen@gmail.com>
2026-05-17 07:55:22 -0400

    Update PageTitle.tsx
    Attempting to add imagei to side pannel

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,9 +7,42 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <div class={classNames(displayClass, "page-title-layout")} style={{ marginBottom: "1.5rem" }}>
+      <h2 class="page-title">
+        <a href={baseDir} style={{ 
+          display: "flex", 
+          alignItems: "center", 
+          gap: "0.75rem", 
+          textDecoration: "none" 
+        }}>
+          {/* Safe inline avatar injection */}
+          <img 
+            src="https://callen-architect.github.io/nexus/static/callenavatar.jpg" 
+            alt="Avatar" 
+            style={{
+              width: "45px",
+              height: "45px",
+              borderRadius: "50%",
+              border: "1.5px solid var(--lightgray)",
+              objectFit: "cover",
+              flexShrink: 0
+            }}
+          />
+          <span>{title}</span>
+        </a>
+      </h2>
+      {/* Subtitle stacked cleanly under the layout alignment */}
+      <span style={{ 
+        display: "block", 
+        fontSize: "0.75rem", 
+        fontWeight: "normal", 
+        color: "var(--gray)",
+        marginTop: "0.35rem",
+        paddingLeft: "calc(45px + 0.75rem)"
+      }}>
+        Atlassian Systems Architect
+      </span>
+    </div>
   )
 }
 
@@ -18,6 +51,9 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+.page-title a:hover {
+  opacity: 0.85;
 }
 `
 
