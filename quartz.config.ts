eaeacc614cfa4cc39897509d8ae7bdcdb22cdfb8
Carl <carl.e.allen@gmail.com>
2026-05-18 05:45:49 -0400

    Update quartz.config.ts
    Rebranded Site.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Atlassian Systems Architect",
+    pageTitle: "Atlassian Codex",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
